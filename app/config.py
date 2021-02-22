af9eca58bc00ae7990a8884a2ee68a34c0f7e9ec
Justin Ho Lung <justin.holung@gmail.com>
2021-02-21 20:49:49 -0500

    lab 3 commit

--- app/config.py
+++ app/config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
-    MAIL_PORT = os.environ.get('MAIL_PORT') or '25'
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sadsfosaifbasbffud'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.mailtrap.io'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or '465'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '026a4b179b420d'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '3a5a316e152084'
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
